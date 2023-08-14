1116c47fde7890253a965d378fcbd512ed9f6ea5
Ramiro <rrcolumbia21@gmail.com>
2023-08-14 08:49:43 +0100

    initialise

--- game.js
+++ game.js
@@ -1,3 +1,16 @@
 // IMPORTANT: Kaboom version 0.5.0 script tag in the HTML file
 
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale: 2,
+    debug: true,
+    clearColor: [0, 0, 0, 1],
+  })
 
+  scene("game", () => {
+
+  })
+
+  start("game")
+  
